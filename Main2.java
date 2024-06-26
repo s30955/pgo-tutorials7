b4cabb7eaa9a6e4a65dd43887c03fd427d209e26
s30955 <163757932+s30955@users.noreply.github.com>
2024-06-26 21:20:27 +0200

    Zadanie 3
    Zrobione zadanie 3, dopisane do zadania 2

--- Main2.java
+++ Main2.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main2 {
     public static void main(String[] args){
@@ -23,5 +21,21 @@ public class Main2 {
         for (Map.Entry<Integer, String> entry : itemMap.entrySet()) {
             System.out.println("Item id: " + entry.getKey() + ", Item name: " + entry.getValue());
         }
+
+        ArrayList<Item> itemsMore = new ArrayList<>();
+        itemsMore.add(new Item("Mouse", 6));
+        itemsMore.add(new Item("Keyboard", 7));
+        itemsMore.add(new Item("Disc", 8));
+        itemsMore.add(new Item("Pendrive", 9));
+        itemsMore.add(new Item("Screen", 10));
+        itemsMore.add(new Item("Speakers", 11));
+        itemsMore.add(new Item("Printer", 12));
+        itemsMore.add(new Item("Scanner", 13));
+        itemsMore.add(new Item("Camera", 14));
+        itemsMore.add(new Item("Microphone", 15));
+
+        itemsMore.subList(0,5);
+
+        HashSet<Item> hashSet = new HashSet<Item>(itemsMore);
     }
 }
